83b58e288f296c70fd0f3942e3060d753eeb1039
stanalexandrucode <stanalexandrucode@gmail.com>
2020-05-11 11:07:21 +0300

    first

---
+++ data_manager.py
@@ -0,0 +1,20 @@
+import database_common
+
+
+@database_common.connection_handler
+def get_comments(cursor):
+    query = """
+        SELECT id, name, text
+        FROM comment
+        ORDER BY id DESC"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def create_comment(cursor, name, text):
+    query = f"""INSERT INTO comment (name, text) 
+                VALUES (%(name)s, %(text)s)"""
+    data = {'name': name, 'text': text}
+
+    cursor.execute(query, data)
